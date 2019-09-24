d8ea42c6db9ebb740db6311716a6ec3be9cd9fa8
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-09-24 16:11:12 -0400

    Add dummy HTTP error handlers

--- backend/app/main/views.py
+++ backend/app/main/views.py
@@ -133,3 +133,10 @@ def delete_transaction(id):
 
     return jsonify(t.serialize)
 
+@main.errorhandler(404)
+def page_not_found(e):
+    return "404 - Page not found!"
+
+@main.errorhandler(500)
+def internal_server_error(e):
+    return "500 - Internal server error!"
