6e7cfcfd81e7c324b00a4fa8c708727d3273d3e1
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-10-28 22:19:02 -0400

    Add basic tests for Transaction and TransactionMonth (#12)

--- backend/tests/test_model.py
+++ backend/tests/test_model.py
@@ -1,7 +1,8 @@
 import unittest
+import datetime
 from flask import current_app
 from app import create_app, db
-from app.models import User, Transaction
+from app.models import User, Transaction, TransactionMonth
 from decimal import *
 
 class ModelTest(unittest.TestCase):
@@ -15,6 +16,9 @@ class ModelTest(unittest.TestCase):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
+
+    # def create_test_transactions(self):
+    #     # create 10 sample transactions
 
     # prefix all test cases with "test_"
     def test_create_user(self):
@@ -39,4 +43,43 @@ class ModelTest(unittest.TestCase):
 
         user = User.query.filter_by(username="jdoe").first()
         self.assertTrue(user is None)
+
+    def test_create_transaction_month(self):
+        # create test user
+        user = User(name="John Doe", username="jdoe", password="goodpass", monthly_budget=50000)
+        db.session.add(user)
+        db.session.commit()
+
+        t_month = TransactionMonth(date=datetime.date(2019, 11, 1), user_id=user.id)
+        db.session.add(t_month)
+        db.session.commit()
+
+        t_months = TransactionMonth.query.all()
+        self.assertTrue(t_months[0] is not None)
+        self.assertEqual(t_months[0].date, datetime.date(2019, 11, 1))
+
+    def test_create_transaction(self):
+        # create test users
+        user = User(name="John Doe", username="jdoe", password="goodpass", monthly_budget=50000)
+        db.session.add(user)
+        db.session.commit()
+
+        # create TransactionMonth
+        t_month = TransactionMonth(date=datetime.date(2019, 11, 1), user_id=user.id)
+        db.session.add(t_month)
+        db.session.commit()
+
+        date = datetime.datetime(2019, 11, 3)
+        t = Transaction(title="Groceries", source="Safeway", amount=10145, date=date, user_id=user.id, transaction_month_id=t_month.id)
+
+        db.session.add(t)
+        db.session.commit()
+
+        user = User.query.filter_by(username="jdoe").first()
+        t = user.transactions[0]
+        self.assertTrue(t is not None)
+        self.assertEqual(t.amount, 10145)
+        self.assertEqual(t.source, "Safeway")
+        self.assertEqual(t.title, "Groceries")
+
     
