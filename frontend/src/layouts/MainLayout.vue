74beb868919b3fb3a8aaf4d4c9b57187c932f432
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-11-10 18:11:30 -0500

    Remove starter code from Vue components

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>budgie</q-toolbar-title>
+
+        <div>{{ $store.state.currentUser.name }}</div>
+        <q-btn
+          flat
+          dense
+          round
+          icon="settings"
+          aria-label="Settings"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
