cb3e4f430dd2bd5eb1cddf52923afa3dbcb6632d
kraglalbert <albert.kragl@mail.mcgill.ca>
2020-05-20 19:46:23 -0700

    Add interceptor to axios for expired access tokens

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import axios from "axios";
+import Store from "../store";
+import Router from "../router";
 
 var config = require("../config");
 
@@ -7,7 +9,7 @@ var config = require("../config");
 const frontendUrl = config.build.host + ":" + config.build.port;
 const backendUrl = config.build.backendHost + ":" + config.build.backendPort;
 
-Vue.prototype.$axios = axios.create({
+const AXIOS = axios.create({
   baseURL: backendUrl,
   headers: {
     "Access-Control-Allow-Origin": frontendUrl,
@@ -15,3 +17,35 @@ Vue.prototype.$axios = axios.create({
   },
   withCredentials: true,
 });
+
+// set up interceptor to handle expired access tokens
+AXIOS.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (Store.getters.isLoggedIn) {
+      if (error.response.status === 401) {
+        // get user with stored token
+        AXIOS.post("/auth/token/refresh", null, {
+          headers: {
+            "X-CSRF-TOKEN": Cookies.get("csrf_refresh_token"),
+          },
+        })
+          .then((resp) => {
+            Store.commit("set_user", resp.data.user);
+          })
+          .catch(() => {
+            // token is invalid
+            Store.dispatch("logout").then(Router.push({ path: "/login" }));
+          });
+      } else {
+        Router.push({ path: "/login" });
+      }
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+Vue.prototype.$axios = AXIOS;
